777b1117febe96dc0ff14f5a3b2686c8c84185a9
Matheus7OP <matheus7op@gmail.com>
2017-12-05 09:14:32 -0300

    Adicionei a classe Animal ao projeto

--- src/hotel/Animal.java
+++ src/hotel/Animal.java
@@ -1,5 +1,41 @@
 package hotel;
 
 public class Animal {
-
+	private String nome;
+	private String tipo;
+	private int idade;
+	
+	public Animal(String nome, String tipo, int idade) {
+		if( nome == null || tipo == null ) {
+			throw new NullPointerException("Parâmetro nulo!");
+		}
+		
+		this.nome = nome;
+		this.tipo = tipo;
+		this.idade = idade;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String getTipo() {
+		return this.tipo;
+	}
+	
+	public int getIdade() {
+		return this.idade;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public void setTipo(String novoTipo) {
+		this.tipo = novoTipo;
+	}
+	
+	public void setIdade(int novaIdade) {
+		this.idade = novaIdade;
+	}
 }
