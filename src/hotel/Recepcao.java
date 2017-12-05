9304736a5c7ab96d81ee3cd7d0b3eee4080a1e5d
Pigm3u <gabrielgtt@hotmail.com>
2017-12-05 09:31:33 -0300

    Adicionando classe Recepcao

---
+++ src/hotel/Recepcao.java
@@ -0,0 +1,48 @@
+package hotel;
+
+import java.util.ArrayList;
+
+public class Recepcao {
+	
+	private ArrayList<Estadia> estadias;
+	
+	public Recepcao() {
+		this.estadias = new ArrayList<Estadia>();
+	}
+	
+	public void checkIn(String nome, String tipo, int idade, int dias, double valor) {
+		Estadia estadia = new Estadia(nome, tipo, idade, dias, valor);
+		estadias.add(estadia);
+	}
+	
+	public void checkOut(String nome) {
+		for (Estadia estadia : estadias) {
+			if (estadia.getNome().equals(nome)) {
+				estadias.remove(estadia);
+				break;
+			}
+		}
+	}
+	
+	public int getNumDeHospedes() {
+		return estadias.size();
+	}
+	
+	public double getLucroTotal() {
+		double total = 0.0;
+		for (Estadia estadia : estadias) {
+			total += estadia.getValor();
+		}
+		return total;
+	}
+	
+	public String toString() {
+		String hospedados = "Estadias:" + System.lineSeparator();
+		for (Estadia estadia : estadias) {
+			total += String.format("%s (%s): %d dias com o preço de R$ %.2f",
+					estadia.getNome(), estadia.getTipo(), estadia.getDias(), estadia.getValor()) 
+					+ System.lineSeparator();
+		}
+		return hospedados;		
+	}
+}
